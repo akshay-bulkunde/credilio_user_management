f6a757c2f1c0139cb720b8a18a05926e741aedca
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-11-22 21:31:18 +0530

    Add routes for user profile management

--- start/routes.ts
+++ start/routes.ts
@@ -9,11 +9,13 @@
 
 import router from '@adonisjs/core/services/router'
 const AuthController = () => import('#controllers/auth_controller')
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
+const ProfilesController = () => import('#controllers/profiles_controller')
 router.post('/register', [AuthController, 'register'])
 router.post('/login', [AuthController, 'login'])
+router.post('/logout', [AuthController, 'logout'])
+
+// Profile routes
+router.post('/user/profile', [ProfilesController, 'create'])
+router.get('/user/profile', [ProfilesController, 'view'])
+router.put('/user/profile', [ProfilesController, 'update'])
+router.delete('/user/profile', [ProfilesController, 'delete'])
